359cfc0a3e3487b3e39516b0ca5f96ffa91cd061
akuznetsov <ajusmith@gmail.com>
2021-09-24 00:13:51 +0300

    feat(hw02) added the Unpack func

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"log"
+	"strconv"
+	"strings"
+	"unicode"
 )
+
+var lastRune rune
+var result strings.Builder
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(PackedString string) (string, error) {
+
+	for i, currentRune := range PackedString {
+
+		if unicode.IsDigit(currentRune) && i == 0 {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsLetter(currentRune) {
+			result.WriteRune(currentRune)
+			lastRune = currentRune
+		}
+
+		if unicode.IsDigit(currentRune) {
+			runesCount, err := strconv.Atoi(string(currentRune))
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if runesCount < 1 {
+				str := result.String()
+				if len(str) > 0 {
+					str = str[:len(str)-1]
+					result.Reset()
+					result.WriteString(str)
+				}
+			}
+
+			for j := 0; j < runesCount-1; j++ {
+				result.WriteRune(lastRune)
+			}
+		}
+	}
+
+	return result.String(), nil
 }
